0484a87bf6940efdebd4045e091c16fdce0d14be
Andris Shvarcs <andris.svarcs@yahoo.com>
2023-11-04 12:34:04 +0000

    Under construction page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class='wrap'>
+	<div>
+		<h1>Shvarcs</h1>
+		<p>Under construction</p>
+	</div>
+</div>
+
+<style lang='scss'>
+  .wrap {
+    display: flex;
+    justify-items: center;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+    background: linear-gradient(135deg, white 50%, black 50%);
+
+    > div {
+      margin: auto;
+    }
+  }
+
+  h1 {
+    font-size: clamp(4rem, 25vw, 25rem);
+    font-weight: bold;
+    justify-self: center;
+		margin-top: -10rem;
+
+    color: #fff;
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    -webkit-text-stroke: 2px #909090;
+  }
+
+	p {
+    color: #909090;
+	}
+</style>
