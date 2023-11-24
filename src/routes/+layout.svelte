1623588c578cd4563eaefaa9dd264ed6a236d852
andris.svarcs <andris.svarcs@yahoo.com>
2023-11-24 20:53:29 +0000

    Design improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import "./globalStyles.css";
+	import "./temp-style.css"
 </script>
 
 <div class='page'>
--- src/routes/news-archive/+layout.svelte
+++ src/routes/news-archive/+layout.svelte
@@ -1,6 +1,16 @@
-<script>
-	import './news-styles.css';
-</script>
+<div class="wrapNews">
+    <div>
+        <slot/>
+    </div>
+</div>
 
-<slot />
+<style lang='scss'>
+  .wrapNews {
+    display: grid;
+    grid-template-columns: 1fr min(45rem, 90%) 1fr;
 
+    > div {
+      grid-column: 2;
+    }
+  }
+</style>
