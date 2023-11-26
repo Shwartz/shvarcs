c553366c694f6af337b095fc10002f4d9f25f6c2
andris.svarcs <andris.svarcs@yahoo.com>
2023-11-26 12:33:09 +0000

    Changing order of Meta tags due to SvelteKit bug

---
+++ src/lib/components/Meta.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let title = '';
+    export let description = '';
+</script>
+
+<title>{title}</title>
+<meta name="title" content="{title}" />
+<meta name="description" content="{description}" />
+<meta property="og:title" content="{title}" />
+<meta property="og:description" content="{description}" />
+<meta property="twitter:title" content="{title}" />
+<meta property="twitter:description" content="{description}" />
