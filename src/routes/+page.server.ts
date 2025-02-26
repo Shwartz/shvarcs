b1eb244edbb169bc92920014ebeb4723fc4264e1
andris.svarcs <andris.svarcs@yahoo.com>
2025-02-26 14:01:14 +0000

    Background grid colour change for dark theme

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
 import { getLastPostsByNumber } from '$lib/notion';
 
 export const load = async () => {
-	const posts = await getLastPostsByNumber(4);
+	// const posts = await getLastPostsByNumber(4);
 	// console.log('page.server.ts:', posts);
 	return {
 		title: 'Frontend stories By Andris',
 		description: 'Stories about CSS, HTML, animation and all around Frontend',
-		posts,
+		posts: getLastPostsByNumber(4),
 	};
 };
