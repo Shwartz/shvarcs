be4ed34ea58fdba04775040c41eba6acef07ff70
andris.svarcs <andris.svarcs@yahoo.com>
2025-04-18 18:49:24 +0100

    Add escapeHTML utility to add ability to show tag examples in a blog

--- src/lib/utils/snippets.ts
+++ src/lib/utils/snippets.ts
@@ -6,3 +6,12 @@ type ImageVariant = '320w' | '640w' | '768w' | '1024w' | '1920w' | '2560w' | 'pu
 export function createImgUrl(imgId: string, size: ImageVariant) {
 	return `https://imagedelivery.net/OsbYeWCzhRDS5xpqlDmBXA/${imgId}/${size}`;
 }
+
+export function escapeHtml(str: string): string {
+	return str
+	.replace(/&/g, '&amp;')
+	.replace(/</g, '&lt;')
+	.replace(/>/g, '&gt;')
+	.replace(/"/g, '&quot;')
+	.replace(/'/g, '&#39;');
+}
